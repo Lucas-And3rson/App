670d6d2f14a5f14596815a8f6ac39a80301727d1
Antonynns <117605065+Lucas-And3rson@users.noreply.github.com>
2023-10-04 10:21:37 -0300

    fluxo de caixa

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -8,5 +8,40 @@ import { Component } from '@angular/core';
 export class Tab4Page {
 
   constructor() {}
+  
+  display = 'none'
+  abrir(){
+    this.display='block';
+  }
+  fechar(){
+    this.display='none';
+  }
+  Cor = "primary"
+  Entrada = 0
+  Total = 0
+  add(){
+      this.Entrada.toFixed(2);
+      this.Entrada++
+      this.Total++
+      if(this.Total>=0) {
+        this.Cor="primary"
+      }
+    }
+
+  Saida = 0
+  ret(){
+    this.Saida++
+    this.Total--
+    if(this.Total<0) {
+      this.Cor="danger"
+    }
+    }
+
+  zerar(){
+    this.Saida=0
+    this.Total=0
+    this.Entrada=0
+    this.Cor="primary"
+    }
 
 }
