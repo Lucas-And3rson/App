0baac39845e1b1231dd20cbe31591e7c1ef6de73
Antonynns <117605065+Lucas-And3rson@users.noreply.github.com>
2023-11-08 10:16:35 -0300

    alunos

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-tab1',
@@ -9,7 +10,26 @@ export class Tab1Page {
   esc:string = ""
   alunos = new Array<Aluno>()
 
-  constructor() {
+  constructor(private storage: Storage) {
+    this.init()
+    this.carregar()
+  }
+  async init(){
+    this.storage = await this.storage.create();
+  }
+  carregar(){
+    this.storage.get('alunos-salvos').then((value) => {
+      if (value !== null) {
+        this.alunos = value
+      }
+    })
+    .catch(() =>{
+      console.log('Deu erro')
+    })
+  }
+
+  persistir(){
+    this.storage.set('alunos-salvos', this.alunos)
   }
 
   escolha(opcao:string){
@@ -20,12 +40,17 @@ export class Tab1Page {
   nota:number = 0
   cadastrarAlunos(){
     this.alunos.push(new Aluno(this.nome, this.nota))
+    this.persistir()
   }
   apagar(posicao: number){
     this.alunos.splice(posicao, 1);
+    this.persistir()
+  }
+  apagarTudo(){
+    this.alunos.length = 0
+    this.persistir()
   }
   }
-
   export class Aluno {
     private nome: string;
     private nota: number;
