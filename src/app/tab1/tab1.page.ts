8086a35975b087f10ab981fad2c1801fc6a3d757
ricardo <ricardo.emanuel66@gmail.com>
2023-12-13 08:26:07 -0300

    camera

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,69 +1,38 @@
 import { Component } from '@angular/core';
-import { Storage } from '@ionic/storage-angular';
+import { PhotoService } from '../services/photo.service';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+selector: 'app-tab1',
+templateUrl: 'tab1.page.html',
+styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  alunos: Aluno[] = [];
-  nome: string;
-  media: number;
-  constructor(private storage: Storage) {
-    this.nome = ''
-    this.media = 0
-    this.init()
-    this.carregarAlunos()
-  }
-  async init(){
-    this.storage = await this.storage.create();
-  }
-  carregarAlunos(){
-    this.storage.get('alunos-salvos').then((value) => {
-      if (value !== null) {
-        this.alunos = value
-      }
-    })
-    .catch(() =>{
-      console.log('Deu erro')
-    })
-  }
+dataHoje:string = "Data: ";
+hora:string = "Hora: ";
+latitude: any;
+longitude: any;
+constructor(public photoService: PhotoService) {}
+addPhotoToGallery() {
+this.photoService.addNewToGallery()
+this.getLocalizacao()
+}
+getLocalizacao(){
+  let data = new Date();
+  let dia = data.getDate();
+  let mes = data.getMonth() + 1;
+  let ano = data.getFullYear();
+  this.dataHoje = "Data: " + dia + "/" + mes + "/" + ano;
+  let hora = data.getHours();
+  let minuto = data.getMinutes();
+  this.hora = "Hora: " + hora + ":" + minuto;
+  Geolocation.getCurrentPosition().then((coordenadas) => {
+  this.latitude = coordenadas.coords.latitude;
+  this.longitude = coordenadas.coords.longitude;
+  })
+  .catch((erro) => {
+  console.log(erro)
+  })
 
-  cadastrarAlunos(){
-    const aluno = new Aluno(this.nome,this.media);
-    aluno.nome = this.nome;
-    aluno.media = this.media;
-    this.alunos.push(aluno);
-    this.persistirAlunos();
   }
-
-  persistirAlunos(){
-    this.storage.set('alunos-salvos', this.alunos)
-  }
-
-  apagarAluno(posicao: number){
-    this.alunos.splice(posicao, 1);
-    this.persistirAlunos();
-  }
-  apagarTodos(){
-    this.alunos.length = 0;
-    this.persistirAlunos();
-  }
-  }
-
-  export class Aluno {
-    public nome: string;
-    public media: number;
-
-    constructor(nome: string, media: number){
-      this.nome = nome;
-      this.media = media
-    }
-    // public getNome() {
-    //   return this.nome;
-    // }
-    // public getMedia() {
-    //   return this.media;
-    // }
-  }
+}
